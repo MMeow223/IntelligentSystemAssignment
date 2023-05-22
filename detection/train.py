3907fd4eb79ac9b21e8b568b190b5391d151fe73
MMeow223 <elviswong2002@hotmail.com>
2023-05-22 15:33:46 +0800

    Add files

--- detection/train.py
+++ detection/train.py
@@ -1,20 +1,10 @@
 from ultralytics import YOLO
 import os
 
-# for files name "train" in folder "D:/AI/runs/detect"
-
 if __name__ == '__main__':
         
-        # Load a model
-        # model = YOLO()
-        
         model = YOLO('D:/IntelligentSystem/YOLOV8/yolov8m.pt')  # load a custom model
-        
-        
-        # export model 
-        # model.export()  # creates onnx, coreml, and torchscript models
-        
-        
+
         # let the model scramble the dataset
         model.train(data="data_custom.yaml",
                     save=True,
@@ -26,16 +16,4 @@ if __name__ == '__main__':
                     lr0=0.0005,
                     save_period=5
             )
-        
-        # plot the training results in real time
-        # model.plot_results()
-        
-
-        # Validate the model with test data
-        # metrics = model.val(data="D:/IntelligentSystem/YOLOV8/data_custom.yaml",save=True)  # no arguments needed, dataset and settings remembered
-
-
-        # use tensorboard to show the results
-        # result = model.tensorboard()  # plot results to val/train2020-12-07_14-06-01
-        
         
